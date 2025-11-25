14b7a08959c0792a1378a8887c72f1a4f6ed3954
Matus <matusfilo987@gmail.com>
2025-11-25 21:30:18 +0100

    gameObserver and unit tests

--- src/main/java/sk/uniba/fmph/dcs/terra_futura/GameObserver.java
+++ src/main/java/sk/uniba/fmph/dcs/terra_futura/GameObserver.java
@@ -3,30 +3,30 @@ package sk.uniba.fmph.dcs.terra_futura;
 import sk.uniba.fmph.dcs.terra_futura.interfaces.TerraFuturaObserverInterface;
 
 class GameObserver {
-    java.util.Map<Integer, TerraFuturaObserverInterface> observers;
+    private final java.util.Map<Integer, TerraFuturaObserverInterface> observers;
 
     GameObserver() {
         observers = new java.util.HashMap<>();
     }
 
-    void addObserver(TerraFuturaObserverInterface observer, Integer id) {
-        if(observers.get(id) != null) {
+    void addObserver(final TerraFuturaObserverInterface observer, final Integer id) {
+        if (observers.get(id) != null) {
             throw new IllegalArgumentException("Observer with id " + id + " already exists!");
         }
         observers.put(id, observer);
     }
 
-    void removeObserver(int id) {
-        if(observers.get(id) == null) {
+    void removeObserver(final int id) {
+        if (observers.get(id) == null) {
             throw new IllegalArgumentException("Observer with id " + id + " does not exist!");
         }
         observers.remove(id);
     }
 
-    void notifyAllNewState(java.util.Map<Integer, String> state) {
+    void notifyAllNewState(final java.util.Map<Integer, String> state) {
 
-        for(Integer key : state.keySet()) {
-            if(observers.get(key) != null) {
+        for (Integer key : state.keySet()) {
+            if (observers.get(key) != null) {
                 observers.get(key).notify(state.get(key));
             }
         }
