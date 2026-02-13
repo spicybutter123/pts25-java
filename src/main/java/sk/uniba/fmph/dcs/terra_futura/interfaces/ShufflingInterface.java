80aa2b41313e4a7471be9e46e59eb119d5cedc40
Michal Tkacik <miskotkacik6@gmail.com>
2026-02-13 07:01:29 +0100

    Add ShufflingInterace.java

---
+++ src/main/java/sk/uniba/fmph/dcs/terra_futura/interfaces/ShufflingInterface.java
@@ -0,0 +1,8 @@
+package sk.uniba.fmph.dcs.terra_futura.interfaces;
+
+import java.util.List;
+import sk.uniba.fmph.dcs.terra_futura.Card;
+
+public interface ShufflingInterface {
+    void shuffle(List<Card> cards);
+}
