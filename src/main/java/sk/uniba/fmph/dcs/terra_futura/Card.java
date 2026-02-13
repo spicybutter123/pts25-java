f34f7742480bfe187818c4dcf2b2aef4565cd84d
Michal Tkacik <miskotkacik6@gmail.com>
2026-02-13 14:15:12 +0100

    fix Card.java

--- src/main/java/sk/uniba/fmph/dcs/terra_futura/Card.java
+++ src/main/java/sk/uniba/fmph/dcs/terra_futura/Card.java
@@ -43,6 +43,7 @@ public class Card implements Rewardable {
      * Zisti ci mozeme zobrat dane resources z kraty (musi ich obsahovat a zaroven
      * nemoze byt pollutnuta).
      * Ak je pollutnuta tak vieme zobrat iba pollution.
+     * 
      * @return true ak sa daju zobrat resources
      * @param resources resources ktore chceme zobrat
      **/
@@ -68,6 +69,7 @@ public class Card implements Rewardable {
 
     /**
      * Odoberie dane resources z karty ak je to mozne.
+     * 
      * @param resources resources ktore odoberame
      **/
     public void getResources(final List<Resource> resources) throws IllegalArgumentException {
@@ -83,7 +85,8 @@ public class Card implements Rewardable {
     /**
      * Zisti ci mozeme pridat dane resources na kartu. (iba zisti ci nie je karta
      * nie je pollutnua).
-     * @return  vrati true ak mozme na kartu polozit resources
+     * 
+     * @return vrati true ak mozme na kartu polozit resources
      * @param resources resources ktore chceme polozit
      **/
     public boolean canPutResources(final List<Resource> resources) {
@@ -92,6 +95,7 @@ public class Card implements Rewardable {
 
     /**
      * Prida dane resources na kartu ak nie je pollutnuta.
+     * 
      * @param resources resources ktore chceme polozit na kartu
      **/
     public void putResources(final List<Resource> resources) throws IllegalArgumentException {
@@ -107,6 +111,7 @@ public class Card implements Rewardable {
     /**
      * Vrati vsetky resources na karte vratane vsetkych pollutions na tejto karte.
      * (nic nezmeni, iba vrati list).
+     * 
      * @return vrati zoznam {@code Resources} ktore su na karte
      **/
     public List<Resource> resourcesOnCard() {
@@ -122,10 +127,11 @@ public class Card implements Rewardable {
     /**
      * Zisti ci je mozne vyrobit pouzitim horneho efektu karty z danych input
      * resources output resources.
+     * 
      * @return true ak vrchny efekt akceptuje parametre
      * @param pollution pollution ktoru ma effekt akceptivat
-     * @param input {@code List<Resources>} ktore ma efekt vediet zobrat
-     * @param output {@code List<Resources>} ktore ma efekt vedfiet vratit
+     * @param input     {@code List<Resources>} ktore ma efekt vediet zobrat
+     * @param output    {@code List<Resources>} ktore ma efekt vedfiet vratit
      **/
     public boolean checkUpper(final List<Resource> input, final List<Resource> output, final int pollution) {
         return upperEffect.isPresent() && upperEffect.get().check(input, output, pollution);
@@ -134,10 +140,11 @@ public class Card implements Rewardable {
     /**
      * Zisti ci je mozne vyrobit pouzitim dolneho efektu karty z danych input
      * resources output resources.
+     * 
      * @return true ak spodny efekt akceptuje parametre
      * @param pollution pollution ktoru ma effekt akceptivat
-     * @param input {@code List<Resources>} ktore ma efekt vediet zobrat
-     * @param output {@code List<Resources>} ktore ma efekt vedfiet vratit
+     * @param input     {@code List<Resources>} ktore ma efekt vediet zobrat
+     * @param output    {@code List<Resources>} ktore ma efekt vedfiet vratit
      **/
     public boolean checkLower(final List<Resource> input, final List<Resource> output, final int pollution) {
         return lowerEffect.isPresent() && lowerEffect.get().check(input, output, pollution);
@@ -145,6 +152,7 @@ public class Card implements Rewardable {
 
     /**
      * Zisti ci spodny alebo dolny efekt karty obsahuje asistenciu.
+     * 
      * @return true ak jeden z efektov ma asisetenciu
      **/
     public boolean hasAssistance() {
@@ -168,7 +176,8 @@ public class Card implements Rewardable {
         return json.toString();
     }
 
-    private static void putOptionalEffectToJSON(final JSONObject json, final String key, final Optional<Effect> effect) {
+    private static void putOptionalEffectToJSON(final JSONObject json, final String key,
+            final Optional<Effect> effect) {
         if (effect.isPresent()) {
             json.put(key, Optional.of(effect.get().state()));
         } else {
@@ -178,6 +187,7 @@ public class Card implements Rewardable {
 
     /**
      * Zisti ci karta je cist (teda ci neobsahuje viac pollution ako moze).
+     * 
      * @return true ak nie je blok pollution
      **/
     public boolean isClear() {
